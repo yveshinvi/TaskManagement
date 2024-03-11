3b4831e5bcef2f3a91cc59aca6366f975d9ec618
yveshinvi <yveshinvi@gmail.com>
2024-03-11 17:07:59 -0400

    commit message

---
+++ gestion_tache/gestion_tache/src/test/java/com/consulting/gestion_tache/GestionTacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consulting.gestion_tache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ gestion_tache/src/main/java/com/consulting/gestion_tache/GestionTacheApplication.java
@@ -0,0 +1,13 @@
+package com.consulting.gestion_tache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionTacheApplication.class, args);
+	}
+
+}
